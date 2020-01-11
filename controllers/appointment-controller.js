bfa43b869191d805c562818a35fc04c8f1526536
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2020-01-11 15:33:28 -0600

    Finished update and delete methods

--- controllers/appointment-controller.js
+++ controllers/appointment-controller.js
@@ -4,9 +4,34 @@ module.exports = app => {
   app.get('/appointments', (req, res) => {
     appointmentDAO
       .findAll()
-      .then(appointments => {
-        res.json(appointments);
-      })
+      .then(appointments => res.json(appointments))
+      .catch(error => res.status(404).json(error));
+  });
+
+  app.get('/appointments/:id', (req, res) => {
+    const id = req.params.id;
+
+    appointmentDAO
+      .findById(id)
+      .then(appointments => res.json(appointments))
+      .catch(error => res.status(404).json(error));
+  });
+
+  app.delete('/appointments/:id', (req, res) => {
+    const id = req.params.id;
+
+    appointmentDAO
+      .delete(id)
+      .then(res.status(204).end())
+      .catch(error => res.status(404).json(error));
+  });
+
+  app.put('/appointments/:id', (req, res) => {
+    const id = req.params.id;
+
+    appointmentDAO
+      .update(id, req.body)
+      .then(result => res.status(201).json(result))
       .catch(error => res.status(404).json(error));
   });
 
--- model/appointment-dao.js
+++ model/appointment-dao.js
@@ -3,42 +3,42 @@ import moment from 'moment';
 
 class AppointmentDAO {
   save(appointment) {
-    const date_create = moment().format('YYYY-MM-DD HH:MM:SS');
-    const date_appointment = moment(appointment.date_appointment, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
-    const appointmentWithDate = { ...appointment, date_create, date_appointment };
+    const date_create = moment().format('YYYY-MM-DD HH:mm:ss');
+    const appointmentWithDate = { ...appointment, date_create };
 
-    // TODO Consider moving validation to a service layer
-
-    /** Init validations */
-
-    const isDateValid = moment(date_appointment).isSameOrAfter(date_create);
-    const isClientValid = appointment.client.length >= 5;
-
-    const validations = [
-      {
-        name: 'date',
-        isValid: isDateValid,
-        message: 'Date must be higher or equal than the current date'
-      },
-      {
-        name: 'client',
-        isValid: isClientValid,
-        message: 'Name must have at least 5 characters'
-      }
-    ];
-
-    const errors = validations.filter(field => !field.isValid);
-
-    //** Finish validations */
-
-    const sql = 'INSERT INTO appointment SET ?';
+    const errors = this.validate(appointment);
 
     return new Promise((resolve, reject) => {
+      const sql = 'INSERT INTO appointment SET ?';
+
       if (errors.length > 0) {
         return reject(errors.map(error => error.message));
       }
 
       connection.query(sql, appointmentWithDate, error => {
+        if (error) {
+          return reject(error.sqlMessage);
+        }
+
+        return resolve(appointmentWithDate);
+      });
+    });
+  }
+
+  update(id, appointment) {
+    const date_create = moment().format('YYYY-MM-DD HH:mm:ss');
+    const appointmentWithDate = { ...appointment, date_create };
+
+    const errors = this.validate(appointment);
+
+    return new Promise((resolve, reject) => {
+      const sql = 'UPDATE appointment SET ? where id = ?';
+
+      if (errors.length > 0) {
+        return reject(errors.map(error => error.message));
+      }
+
+      connection.query(sql, [appointmentWithDate, id], error => {
         if (error) {
           return reject(error.sqlMessage);
         }
@@ -54,11 +54,60 @@ class AppointmentDAO {
     return new Promise((resolve, reject) => {
       connection.query(sql, (error, result) => {
         if (error) {
-          return reject('Error when getting appointments from database: ' + error.sqlMessage);
+          return reject(`Error when getting appointments from database: ${error.sqlMessage}`);
         }
         return resolve(result);
       });
     });
+  }
+
+  findById(id) {
+    const sql = 'SELECT * FROM appointment where id = ?';
+
+    return new Promise((resolve, reject) => {
+      connection.query(sql, id, (error, result) => {
+        if (error) {
+          return reject(`Error when getting appointment with id: ${id} - Error: ${error.sqlMessage}`);
+        }
+        return resolve(result[0]);
+      });
+    });
+  }
+
+  delete(id) {
+    const sql = 'DELETE FROM appointment where id = ?';
+
+    return new Promise((resolve, reject) => {
+      connection.query(sql, id, (error, result) => {
+        if (error) {
+          return reject(`Error deleting appointment with id: ${id} - Error: ${error.sqlMessage}`);
+        }
+        return resolve();
+      });
+    });
+  }
+
+  // TODO Consider moving validation to a service layer
+  validate(fields) {
+    const { date_create, date_appointment, client } = fields;
+
+    const isDateValid = moment(date_appointment).isSameOrAfter(date_create);
+    const isClientValid = client.length >= 5;
+
+    const validations = [
+      {
+        name: 'date',
+        isValid: isDateValid,
+        message: 'Date must be higher or equal than the current date'
+      },
+      {
+        name: 'client',
+        isValid: isClientValid,
+        message: 'Name must have at least 5 characters'
+      }
+    ];
+
+    return validations.filter(field => !field.isValid);
   }
 }
 
